5b34a8b4ba51823f79a624a4675d4cc04a70e8e5
Bruno Kenji <bruno.furukawa@dev.codus.com.br>
2015-10-14 11:09:06 -0300

    calc2 keypress

--- Calculator/ver2/backup/script2.js
+++ Calculator/ver2/backup/script2.js
@@ -1,35 +1,36 @@
 $(document).ready(function(){
     var displayFix = function(number) {
         if (number.length > 9) {
-            totaldiv.text(number.substr(number.length-9,9));
-        } 
+            total.text(number.substr(number.length-9,9));
+        }
     };
     var number = "";
     var newnumber = "";
     var operator = "";
-    var totaldiv = $(".display");
-    totaldiv.text("0");
-    $(".numbers span").not(".clear").click(function(){
+    var total = $(".display");
+    total.text("0");
+
+    $(".numbers span").not(".clear, .dot").click(function(){
         number += $(this).text();
-        totaldiv.text(number);
+        total.text(number);
         displayFix(number);
     });
     $(".operators span").not(".igual").click(function(){
         operator = $(this).text();
         newnumber = number;
         number = "";
-        totaldiv.text("0");
+        total.text("0");
     });
     $(".clear").click(function(){
         number = "";
-        totaldiv.text("0");
+        total.text("0");
         if ($(this).attr("id") === "clearall") {
             newnumber = "";
         }
     });
     $(".igual").click(function(){
         if (operator === "+"){
-            number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
+            number = (parseInt(newnumber,10) + parseInt(number, 10)).toString(10);
         } else if (operator === "-"){
             number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
         } else if (operator === "/"){
@@ -37,9 +38,56 @@ $(document).ready(function(){
         } else if (operator === "*"){
             number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
         }
-        totaldiv.text(number);
+        total.text(number);
         displayFix(number);
         number = "";
         newnumber = "";
+    });
+    $(".dot").click(function() {
+        if ( number.length == 0)     
+    { number = "0.";
+    } else
+    {  if ( number.indexOf(".") == -1)
+         { number = number + ".";
+    };   };
+  total.text(number);
+    });
+        $(document).keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode === 49) {
+            $("#num1").click();
+       } else if (keycode === 50) {
+            $("#num2").click();
+        } else if (keycode === 51) {
+            $("#num3").click();
+        } else if (keycode === 52) {
+            $("#num4").click();
+        } else if (keycode === 53) {
+            $("#num5").click();
+        } else if (keycode === 54) {
+            $("#num6").click();
+        } else if (keycode === 55) {
+            $("#num7").click();
+        } else if (keycode === 56) {
+            $("#num8").click();
+        } else if (keycode === 57) {
+            $("#num9").click();
+        } else if (keycode === 48) {
+            $("#num0").click();
+        } else if (keycode === 99 || keycode === 127 || keycode === 08) {
+            $(".clear").click();
+        } else if (keycode === 46) {
+            $(".dot").click();
+        } else if (keycode === 61 || keycode === 13) {
+            $(".igual").click();
+        } else if (keycode === 43) {
+            $("#plus").click();
+        } else if (keycode === 45) {
+            $("#minus").click();
+        } else if (keycode === 42 || keycode === 120) {
+            $("#multiply").click();
+        } else if (keycode === 47) {
+            $("#divide").click();
+        }
     });
 });
