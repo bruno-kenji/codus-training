05ab7899643f55bcea93728d75ae2f8ab7c105ae
Bruno Kenji <bruno.furukawa@dev.codus.com.br>
2015-10-20 14:02:54 -0200

    speed adjusted, code more clean

--- Solid/Snake/script2.js
+++ Solid/Snake/script2.js
@@ -1,11 +1,18 @@
 function gameStart() {	
 	for(row=0;row<20;row++)for(x=0;x<32;x++)$(row+","+x).style.background='#f1f1f1';
-	snk1.Body=[[8,0],[8,0],[8,0],[8,0],[8,1]];
-	snk1.dir=1;
-	snk1.points=0;
+	snk.Body=[[8,0],[8,0],[8,0],[8,0],[8,1]];
+	snk.dir=1;
+	snk.points=0;
+	Stage=0;
 	spawnFood();
 	$('menu').innerHTML="Stage "+Stage;
 	movement();
+}
+
+function stageIncrease() {
+	Stage++;
+	speeds[Stage];
+	$('menu').innerHTML="Stage "+Stage;
 }
 
 function spawnFood() {
@@ -13,8 +20,8 @@ function spawnFood() {
 		x=Math.floor(Math.random()*20);
 		y=Math.floor(Math.random()*32);
 		var flag=false;
-		for(n=0;n<snk1.Body.length;n++) {
-			if(snk1.Body[n][0]==x&&snk1.Body[n][1]==y) {
+		for(n=0;n<snk.Body.length;n++) {
+			if(snk.Body[n][0]==x&&snk.Body[n][1]==y) {
 			flag=true;
 			}
 		}
@@ -22,68 +29,74 @@ function spawnFood() {
 	while(flag);
 	$(x+","+y).style.background='red url(https://s3.amazonaws.com/profile_photos/14464850469542.vPUKNFAoqcAHYnKvw0XG_60x60.png) no-repeat 45% 37%';
 	Food=[x,y];
-	Stage++;
-	speeds[Stage];
-	$('menu').innerHTML="Stage "+Stage;
+	stageIncrease();
 }
 
 function directionInput(arrowDirection) {
 	switch(arrowDirection) {
 		case 37: 
-			if(snk1.Head()[1]!=snk1.Body[snk1.Body.length-2][1]+1) {
-				snk1.dir=3; 
+			if(snk.Head()[1]!=snk.Body[snk.Body.length-2][1]+1) {
+				snk.dir=3; 
 			}
 			break;
 		case 38: 
-			if(snk1.Head()[0]!=snk1.Body[snk1.Body.length-2][0]+1) {
-				snk1.dir=0; 
+			if(snk.Head()[0]!=snk.Body[snk.Body.length-2][0]+1) {
+				snk.dir=0; 
 			}
 			break;
 		case 39: 
-			if(snk1.Head()[1]!=snk1.Body[snk1.Body.length-2][1]-1) {
-				snk1.dir=1; 
+			if(snk.Head()[1]!=snk.Body[snk.Body.length-2][1]-1) {
+				snk.dir=1; 
 			}
 			break;
 		case 40: 
-			if(snk1.Head()[0]!=snk1.Body[snk1.Body.length-2][0]-1) {
-				snk1.dir=2;
+			if(snk.Head()[0]!=snk.Body[snk.Body.length-2][0]-1) {
+				snk.dir=2;
 			}
 			break;
 	}
 }
 function movement() {
-	snk1.fade();
-	snk1.desloca();
-	snk1.move();
-	snk1.eat();
-	snk1.draw();
+	snk.fade();
+	snk.desloca();
+	snk.move();
+	snk.eatAndGrow();
+	snk.draw();
 	flag=false;
-	for(n=0;n<snk1.Body.length-1;n++)
-	{	if(snk1.Head()[0]==snk1.Body[n][0]&&snk1.Head()[1]==snk1.Body[n][1]) flag=1;
+	for(n=0;n<snk.Body.length-1;n++) {
+		if(snk.Head()[0]==snk.Body[n][0]&&snk.Head()[1]==snk.Body[n][1]) {
+			flag=1;
+		}
 	}
-	if(!flag)setTimeout("movement()",Stage<23?speeds[Stage]:30);
+	if(!flag)setTimeout("movement()",Stage<21?speeds[Stage]:30);
 	else
 	{	if(flag==1)
-		{	totalpoints+=snk1.points*10+((Stage-1)*6);
-			$('menu').innerHTML="Game finish.<br>"+"Did "+snk1.points+" points.<br><br>"+"<input type=button value=Start onclick='gameStart();'>";
+		{	totalpoints+=snk.points*10+((Stage-1)*6);
+			$('menu').innerHTML="Game finish.<br>"+"Did "+snk.points+" points.<br><br>"+"<input type=button value=Start onclick='gameStart();'>";
 			Stage=0;
 			totalpoints=0;
 		}
-		else
+		else	
 		{	Stage=1;
-			totalpoints+=snk1.points*10+((Stage-1)*6);
+			totalpoints+=snk.points*10+((Stage-1)*6);
 			gameStart();
 		}
 	}
 }
+
 $=function(e) {
 	return(document.getElementById(e));
 };
+grid="<table align=center cellpadding=0 cellspacing=0 style='border:2px solid #000000;'><tr>";
+for(row=0;row<20;row++) {
+	for(x=0;x<32;x++) {
+		grid+="<td id='"+row+","+x+"'>&nbsp;</td>"+(x==31?"</tr><tr>":"")
+	};
+};
+grid+="</tr></table>";
+$('main').innerHTML=grid;
 
-text="<table align=center cellpadding=0 cellspacing=0 style='border:2px solid #000000;'><tr>";
-for(row=0;row<20;row++)for(x=0;x<32;x++)text+="<td id='"+row+","+x+"'>&nbsp;</td>"+(x==31?"</tr><tr>":"");
-text+="</tr></table>";
-$('main').innerHTML=text;
+
 snakes=function(c) {
 	this.Color=c;
 	this.bodyPiece=function(x) {
@@ -100,7 +113,7 @@ snakes=function(c) {
 			this.Body[n][1]=this.Body[n+1][1];
 		}
 	};
-	this.eat=function()
+	this.eatAndGrow=function()
 	{	if(this.Head()[0]==Food[0]&&this.Head()[1]==Food[1])
 		{	spawnFood();
 			this.Body.splice(0,0,[this.Body[this.Body.length-2][0],this.Body[this.Body.length-2][1]]);
@@ -128,8 +141,8 @@ snakes=function(c) {
 	{	return this.Body[this.Body.length-1];
 	};
 };
-var snk1=new snakes('#333');
+var snk=new snakes('#333');
 var Food;
 var Stage=0;
-var speeds=[220,200,180,165,150,140,130,120,110,100,95,90,85,80,75,70,65,60,55,50,45,40,35];
+var speeds=[200,180,165,150,135,120,120,110,100,95,90,85,80,75,70,65,60,55,50,45,40,35];
 var totalpoints=0;
