972c46ff697d871214747d038254ac01dd5236fb
Bruno Kenji <bruno.furukawa@dev.codus.com.br>
2015-10-26 14:17:24 -0200

    Rock paper scissors game

---
+++ Ruby/Rock_paper_scissors/game.rb
@@ -0,0 +1,40 @@
+class WrongNumberOfPlayersError < StandardError ; end
+class NoSuchStrategyError < StandardError ; end
+
+def result_generator(player1, player2, game)
+	if ( player1 == player2 ) 
+        return "#{game[0]} wins, since #{game[0][1]} = #{game[1][1]}"
+    end
+    if ( player1 == "R" )
+    	if ( player2 == "S" )
+        	return ( "#{game[0]} wins, since #{game[0][1]} > #{game[1][1]}" )
+        else 
+        	return ( "#{game[1]} wins, since #{game[0][1]} < #{game[1][1]}" )
+        end
+    elsif ( player1 == "S" )
+    	if ( player2 == "P" )
+    		return ( "#{game[0]} wins, since #{game[0][1]} > #{game[1][1]}" )
+    	else
+    		return ( "#{game[1]} wins, since #{game[0][1]} < #{game[1][1]}" )
+    	end
+    else
+    	if ( player2 == "R" )
+    		return ( "#{game[0]} wins, since #{game[0][1]} > #{game[1][1]}" )
+    	else
+    		return ( "#{game[1]} wins, since #{game[0][1]} < #{game[1][1]}" )
+    	end
+    end
+end
+
+def rps_game_winner(game)
+    raise WrongNumberOfPlayersError unless game.length == 2
+    player1_strategy = game[0][1].upcase
+    player2_strategy = game[1][1].upcase
+    raise NoSuchStrategyError unless ["R", "S", "P"].include?(player1_strategy)
+    raise NoSuchStrategyError unless ["R", "S", "P"].include?(player2_strategy)
+    result_generator( player1_strategy, player2_strategy, game )
+end
+
+game = [ [ "Armando", "P" ], [ "Dave", "P" ] ]
+
+puts rps_game_winner(game)
