38fd0a7dbc61854e9f28bb3f9e3a121d076f0d60
Bruno Kenji <bruno.furukawa@dev.codus.com.br>
2015-10-26 19:11:09 -0200

    anagrams --in progress

---
+++ Ruby/Anagrams/anagram.rb
@@ -0,0 +1,24 @@
+def combine_anagrams(words)
+	array_of_anagrams = []
+
+	words.each do |word|
+		formatted_word = word.downcase.split("").sort
+
+		array_of_words = []
+
+		words.each do |next_word|
+			formatted_next_word = next_word.downcase.split("").sort
+
+			if (formatted_word == formatted_next_word)
+			array_of_words.push(word)
+			end
+		end
+		array_of_anagrams.push(array_of_words)
+	end
+	return array_of_anagrams
+end
+
+words = ['cars', 'for', 'potatoes', 'Racs', 'four','scar', 'creams',
+'Scream']
+print combine_anagrams(words)
+puts " "
